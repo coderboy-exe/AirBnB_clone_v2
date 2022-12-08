78275da80fce8b12099691906169fc76f46fa455
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-12-08 15:32:15 +0000

    update base_model to use db enging

--- models/city.py
+++ models/city.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+
+    __tablename__ = 'cities'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(60), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    else:
+        name = ""
+        state_id = ""
--- tests/test_console.py
+++ tests/test_console.py
@@ -5,9 +5,18 @@
 import sys
 import models
 import unittest
+from models import State
 from models import storage
+from models.engine.db_storage import DBStorage
 from console import HBNBCommand
 from IO import StringIO
+from os import getenv
+from unittest.mock import create_autospec
+
+
+db = getenv("HBNB_TYPE_STORAGE", "fs")
+
+
 
 class test_console(unittest.TestCase):
     ''' Test the console module'''
@@ -41,3 +50,53 @@ class test_console(unittest.TestCase):
         console.onecmd("all")
         self.assertTrue(isinstance(self.capt_out.getvalue(), str))
 
+    @unittest.skipIf(db == "db", "Testing database storage only")
+    def test_show(self):
+        '''
+            Testing that show exists
+        '''
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        console.onecmd("show User " + user_id)
+        x = (self.capt_out.getvalue())
+        sys.stdout = self.backup
+        self.assertTrue(str is type(x))
+
+    @unittest.skipIf(db == "db", "Testing database storage only")
+    def test_show_class_name(self):
+        '''
+            Testing the error messages for class name missing.
+        '''
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        console.onecmd("show")
+        x = (self.apt_out.getvalue())
+        sys.stdout = self.backup
+        self.assetEqual("** class name missing **\n", x)
+
+    @unittest.skipIf(db == "db", "Testing database storage only")
+    def test_show_no_instance_found(self):
+        '''
+            Test show message error for id missing
+        '''
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.sdout = self.capt_out
+       	console.onecmd("show User " + "1234567")
+	x = (self.capt_out.getvalue())
+        sys.stdout = self.backup
+        self.assetEqual("** no instance found **\n", x)
