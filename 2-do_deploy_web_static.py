b5b54ff70e0da9bad02a96e9081937f826740e34
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-12-22 14:20:42 +0000

    update

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Fabric script that distributes an archive to 2 web servers """
+
+from fabric.api import env, put, run
+import os.path
+
+
+env.hosts = ['18.234.106.217', '100.25.30.148']
+
+
+def do_deploy(archive_path):
+    """
+    Deploy archive to web server
+    """
+    if os.path.exists(archive_path) is False:
+        return False
+
+    try:
+        file_name = archive_path.split("/")[-1]
+        rmv_ext = file_name.split(".")[0]
+        path_rmv_ext = "/data/web_static/releases/{}/".format(rmv_ext)
+        symlink = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_rmv_ext))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, path_rmv_ext))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(path_rmv_ext, path_rmv_ext))
+        run("rm -rf {}web_static".format(path_rmv_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_rmv_ext, symlink))
+        return True
+    except TypeError:
+        return False
